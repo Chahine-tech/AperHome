1bb1c68d4227d64d7de321497fd87449c976ab17
Chahine-tech <chahine.benahcen@gmail.com>
2021-04-01 18:24:27 +0200

    fix config

--- src/server.js
+++ src/server.js
@@ -1,20 +1,38 @@
-import dotenv from 'dotenv'
-dotenv.config()
 import express from 'express'
-const app = express();
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
 import routes from './routes'
+import cors from 'cors'
+import './middlewares/passport'
 
+async function server() {
+    try {
+      
+        const prisma = new PrismaClient()
+        await prisma.$connect()
 
-  
-app.get('/', (_req, res) => {
-    res.send("Please take a look at our <a href='/api'>API</a>")
-  })
+      const app = express();
+      app.use(express.json())
 
-  app.use('/api', routes)
+    app.get('/', (_req, res) => {
+        res.send("Please take a look at our <a href='/api'>API</a>")
+      })
 
+      app.use('/api', routes)
+      
 
-app.listen(process.env.PORT, function() {
-    console.log(`Server starting at http://localhost:${process.env.PORT}`);
-  });
+      dotenv.config()
+    const port = parseInt(process.env.PORT || 3000, 10)
 
+    app.listen(3000, function() {
+        console.log("Server starting at http://localhost:3000");
+      });
+
+    }
+    catch (err) {
+        console.log("error start")
+      }
+    
+}
+
+server()
